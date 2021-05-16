a6a72d2efdb1d4fe41a6feb1d67d475c92c1f244
Yuliya <ylevitsk@aol.com>
2021-05-16 09:25:50 -0700

    update

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -28,13 +28,12 @@ function App() {
         if (i === 0)
           costs[j] = j;
         else if (j > 0) {
-            let newValue = costs[j - 1];
-            if (s1.charAt(i - 1) !== s2.charAt(j - 1))
-              newValue = Math.min(Math.min(newValue, lastValue),
-                  costs[j]) + 1;
-            costs[j - 1] = lastValue;
-            lastValue = newValue;
-          }
+          let newValue = costs[j - 1];
+          if (s1.charAt(i - 1) !== s2.charAt(j - 1))
+            newValue = Math.min(Math.min(newValue, lastValue),
+                costs[j]) + 1;
+          costs[j - 1] = lastValue;
+          lastValue = newValue;
         }
       }
       if (i > 0)
